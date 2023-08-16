d36cf1fc1d99ed95835aa87c7b359f280b47dd82
unknown <milanzec53@gmail.com>
2023-08-16 16:55:42 +0200

    input

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class SearchBarComponent {
 
+  term = ''
+
+  onFormSubmit(event:any) {
+    event.preventDefault()
+    console.log(this.term)
+  }
 }
